a977b3468e6ead26bfbc8e806508732f57f78a03
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-08-08 19:25:37 +0530

    shifting to actual data

--- src/App.js
+++ src/App.js
@@ -2,29 +2,35 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Network from "./components/Network";
 // import FilterPanel from "./components/FilterPanel";
-import data from "./newData.json";
+// import data from "./newData.json";
+
+import { filteredActiveValidators, range, nomRange } from './data/allData';
 
 const App = () => {
   const [graphType, changeGraphType] = useState("Tree");
   const [connectionType, changeConnectionType] = useState("Connected");
   const [disconnectedData, setDisconnectedData] = useState([]);
-  useEffect(() => {
-    const newData = data.map((d) => ({
-      address: d.address,
-    }));
-    setDisconnectedData(newData);
-  }, []);
-  const updateGraph = () =>
-    changeGraphType(graphType === "Graph" ? "Tree" : "Graph");
-  const updateConnection = () =>
-    changeConnectionType(
-      connectionType === "Connected" ? "Disconnected" : "Connected"
-    );
+
+  // useEffect(() => {
+  //   const newData = data.map((d) => ({
+  //     address: d.address,
+  //   }));
+  //   setDisconnectedData(newData);
+  // }, []);
+
+  // const updateGraph = () =>
+  //   changeGraphType(graphType === "Graph" ? "Tree" : "Graph");
+  // const updateConnection = () =>
+  //   changeConnectionType(
+  //     connectionType === "Connected" ? "Disconnected" : "Connected"
+  //   );
   return (
     <div className="App">
       <Network
         graphType={graphType}
-        graphData={connectionType === "Connected" ? data : disconnectedData}
+        graphData={connectionType === "Connected" ? filteredActiveValidators : disconnectedData}
+        range={range}
+        nomRange={nomRange}
       />
       {/* <FilterPanel
         graphType={graphType}
--- src/components/Network.js
+++ src/components/Network.js
@@ -1,53 +1,54 @@
-import React, { useEffect } from "react";
-import cytoscape from "cytoscape";
-import dagre from "cytoscape-dagre";
-import { createCytoscapeConfig, getGraphData } from "../utils";
+import React, { useEffect } from 'react';
+import cytoscape from 'cytoscape';
+import dagre from 'cytoscape-dagre';
+import { createCytoscapeConfig, getGraphData, getRank } from '../utils';
 
 cytoscape.use(dagre);
 
-const Network = ({ graphType, graphData }) => {
+const Network = ({ graphType, graphData, range, nomRange }) => {
   useEffect(() => {
     let cy = cytoscape(
-      createCytoscapeConfig(getGraphData(graphData), graphType)
+      createCytoscapeConfig(getGraphData(graphData, range), graphType)
     );
     // cy.animate().delay(1500).animate({
     //   zoom: 1,
     // });
     let node = cy.elements().nodes();
-    node.unbind("mouseover");
-    node.bind("mouseover", () => {
-      document.body.style.cursor = "pointer";
+    node.unbind('mouseover');
+    node.bind('mouseover', () => {
+      document.body.style.cursor = 'pointer';
     });
-    node.unbind("mouseout");
-    node.bind("mouseout", () => {
-      document.body.style.cursor = "default";
+    node.unbind('mouseout');
+    node.bind('mouseout', () => {
+      document.body.style.cursor = 'default';
     });
 
-    node.bind("click", (event) => {
+    node.bind('click', (event) => {
       const elements = [];
+      console.log(event.target._private.data)
       const { nominators, id } = event.target._private.data;
       nominators.forEach((v) => {
         elements.push({
-          group: "nodes",
+          group: 'nodes',
           data: {
-            id: v.address,
-            bg: "blue",
-            shape: "ellipse",
-            size: v.rank * 10,
-            rank: v.rank,
+            id: v.nomId,
+            bg: 'blue',
+            shape: 'ellipse',
+            size: 15,
+            rank: Math.floor(Math.random() * 4) || getRank(v.stake, nomRange),
           },
         });
         elements.push({
-          group: "edges",
+          group: 'edges',
           data: {
             source: id,
-            target: v.address,
+            target: v.nomId,
           },
         });
       });
       cy.add(elements);
-      var layout = cy.layout({
-        name: "dagre",
+      const layout = cy.layout({
+        name: 'dagre',
         fit: false,
         rankSep: 200,
         transform: function (node, pos) {
@@ -59,7 +60,7 @@ const Network = ({ graphType, graphData }) => {
       cy.zoom(1);
     });
   }, [graphType, graphData]);
-  return <div id="cy" style={{ height: "42rem", position: "relative" }}></div>;
+  return <div id='cy' style={{ height: '42rem', position: 'relative' }}></div>;
 };
 
 export default Network;
